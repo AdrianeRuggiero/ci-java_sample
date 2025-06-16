0c21f5921f42b71703ccf5538913dda634b26af4
AdraneRuggiero <driketes@gmail.com>
2025-06-16 18:33:02 +0200

    Supprime la méthode main non testée

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,6 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, CI/CD World!");
-    }
 
     public static int sum(int a, int b) {
         return a + b;
