70379c574ee3bd58fd57094820ef650eb5e4a838
AdraneRuggiero <driketes@gmail.com>
2025-06-16 18:43:05 +0200

    Fix SonarQube issue: utility class with private constructor

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,9 @@
 package com.example;
 
 public class App {
-
+    private App() {
+        // Classe utilitaire : pas d’instanciation
+    }
     public static int sum(int a, int b) {
         return a + b;
     }
